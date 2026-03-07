ac336c3dd3c8bab2af9d03811f70c8759e8b5fdf
ZerModzTeam <gerap4031@gmail.com>
2026-03-07 11:32:54 +0700

    Delete log.php

--- log.php
+++
@@ -1,5 +0,0 @@
-<?php
-$num = str_replace(["\r", "\n"], '', $_GET['num']);
-$log = "WA: $num | IP: " . $_SERVER['REMOTE_ADDR'] . " | UA: " . $_SERVER['HTTP_USER_AGENT'] . "\n";
-file_put_contents("logs.txt", $log, FILE_APPEND);
-?>
